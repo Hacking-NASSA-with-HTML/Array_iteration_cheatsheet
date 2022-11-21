29c080d80f5a127f3e42752b4b4337fc606aef5d
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-11-22 03:00:38 +0400

    feat: add open graph images

--- mainScript.js
+++ mainScript.js
@@ -5,7 +5,7 @@ generatedContent.innerHTML = mainPageContent()
 function mainPageContent() {
     const renderMainPageContent =
         `<main id="main">
-            <h1 class="titleH1">Array iteration cheat sheet</h1>
+            <h1 class="titleH1">Array Iterations Syntax Cheat sheet</h1>
             <p class="paragraph">
                 <a class="link" title="forEach Method" href="./forEach/forEach.html">forEach Method</a>
             </p>
